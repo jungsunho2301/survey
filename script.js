c1dde47ef12d5dad47c3fd4ce94f10ed32bceaba
jungsunho2301 <jsh5472301@naver.com>
2026-01-20 18:58:55 +0900

    다시 출항경유지역과 출항일로

--- script.js
+++ script.js
@@ -50,12 +50,13 @@ for (let q of requiredQuestions) {
 /* Q5 예일 경우 상세 입력 검사 */
 if (f.get("q5") === "yes") {
   if (!f.get("q5_region")) {
-    missing.push("Q5 승선국가");
+    missing.push("Q5 출항·경유 지역");
   }
   if (!f.get("q5_departure_date")) {
-    missing.push("Q5 승선일자");
+    missing.push("Q5 출항일");
   }
 }
+
 
 if (missing.length > 0) {
   alert(
